26ee861954f9fc972ca663a301003f63f34b080a
Sam Yong <yongmc.sam2014@outlook.com>
2020-02-21 23:34:08 -0500

    global settings listeners

--- ui.js
+++ ui.js
@@ -1,3 +1,24 @@
+/* ui.js
+ * handle app ui rendering and updates
+ * handle global settings
+ * handle ui and backend interactions
+ */
+
+/* constants
+ * - suits_rankings (symbol -> array)
+ * - left_select (element select)
+ * - right_select (element select)
+ * - left_checkbox_label (element label)
+ * - right_checkbox_label (element label)
+ */
+
+/* global settings
+ * - left_lang (object { tetra, suits_ranking, ace_high })
+ * - right_lang (object { ^^ })
+ */
+
+/* ************************************************************************* */
+
 const suits_rankings = {
   alternating: ['D', 'C', 'H', 'S'],
   alphabetical: ['C', 'D', 'H', 'S'],
@@ -24,3 +45,33 @@ right_select.selectedIndex = 0;
 const left_checkbox_label = document.querySelector('label#left-checkbox-label');
 const right_checkbox_label = document.querySelector('label#right-checkbox-label');
 left_checkbox_label.style.display = 'block';
+
+/* ************************************************************************* */
+
+/* settings listeners */
+document.querySelector('input[name="left-checkbox"]').addEventListener('change', function() { /* left checkbox */
+  if (this.checked) {
+    left_lang.ace_high = true;
+  } else {
+    left_lang.ace_high = false;
+  }
+});
+document.querySelector('input[name="right-checkbox"]').addEventListener('change', function() { /* right checkbox */
+  if (this.checked) {
+    right_lang.ace_high = true;
+  } else {
+    right_lang.ace_high = false;
+  }
+});
+left_select.addEventListener('change', function() {
+  const v = this.value;
+  if (v === 'dec') {
+  } else {
+  }
+});
+right_select.addEventListener('change', function() {
+  const v = this.value;
+  if (v === 'dec') {
+  } else {
+  }
+});
